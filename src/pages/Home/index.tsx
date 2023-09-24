222d7618c33ad16cfdaaa1fa04a3215875f16f1f
Phil Ferreira <f.ferreira@qxlogistix.local>
2023-09-24 14:43:26 -0700

    add time

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,23 +6,57 @@ import {zodResolver} from '@hookform/resolvers/zod'
 import * as zod from 'zod'
 
 const newCylceFormSchema = zod.object({
-  task: zod.string().min(1,'Input a task'),
-  minutesAmount: zod.number().min(5).max(60, 'Your cycle can only have 60 minutes at most')
+  task: zod.string().min(1,'Inform a task'),
+  minutesAmount: zod
+  .number()
+  .min(5, 'Your cycle must have 5 minutes at least')
+  .max(60, 'Your cycle must have 60 minutes at most')
 })
+// interface NewCycleFormData{
+//   task: string
+//   minutesAmount: number
+// }
+type NewCycleFormData = zod.infer<typeof newCylceFormSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
 export function Home(){
-// const [task, setTask] = useState('')
-const {register, handleSubmit, watch, formState} = useForm({
+const [cycles, setCycles] = useState<Cycle[]>([])
+const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+const [secsPassed, setSecsPassed] = useState(0)
+const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
   resolver: zodResolver(newCylceFormSchema),
+  defaultValues:{
+    task: '',
+    minutesAmount: 0,
+  }
 })
 const task = watch('task')
 const isSubmitDisabled = !task
 
-function createNewCycle(data:any){
-  console.log(data)
-
+function createNewCycle(data:NewCycleFormData){
+  const newCycle:Cycle={
+    id: String(new Date().getTime()),
+    task: data.task,
+    minutesAmount: data.minutesAmount
+  }
+  setCycles(state => [...state, newCycle])
+  setActiveCycleId(newCycle.id)
+  reset()
 }
+const activeCycle = cycles.find((cycle)=> cycle.id === activeCycleId)
 
+const totalSecs = activeCycle ? activeCycle.minutesAmount * 60 : 0
+const currSecs = activeCycle ? totalSecs - secsPassed : 0
+
+const minsAmount = Math.floor(totalSecs / 60)
+const secsAmount = currSecs %60 
+
+const mins = String(minsAmount).padStart(2,'0')
+const secs = String(secsAmount).padStart(2,'0')
 
   return(
     <HomeContainer>
@@ -57,11 +91,11 @@ function createNewCycle(data:any){
           <span>minutes.</span>
         </FormContainer>
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{mins[0]}</span>
+          <span>{mins[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{secs[0]}</span>
+          <span>{secs[1]}</span>
         </CountdownContainer>
           <StartCountdownBtt disabled={isSubmitDisabled} type="submit">
             <Play size={24}/>
